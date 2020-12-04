4738e65517d5c0df32f449c3be0c61f0ba116c58
unknown <shabankhawar99@gmail.com>
2020-12-04 13:22:12 +0500

    Flatlists

--- App.js
+++ App.js
@@ -1,50 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Dude");
-  const [age, setAge] = useState(21);
-  const [textfieldName, setTextfieldName] = useState("");
-  const [textfieldAge, setTextfieldAge] = useState("");
-  let change = () => {
-    setName(textfieldName);
-    setAge(textfieldAge);
-  };
+  const [data, setData] = useState([
+    { name: "Dude", id: 1 },
+    { name: "Someone", id: 12 },
+    { name: "Broke", id: 13 },
+    { name: "The", id: 14 },
+    { name: "Xbone", id: 15 },
+    { name: "And", id: 16 },
+    { name: "Got", id: 17 },
+    { name: "Away", id: 18 },
+    { name: "With It", id: 19 },
+  ]);
   return (
     <View style={styles.container}>
-      <View style={styles.inputDiv}>
-        <Text>Enter Name</Text>
-        <TextInput
-          multiline
-          style={styles.textInput}
-          placeholder="Your Name"
-          onChangeText={(val) => {
-            setTextfieldName(val);
-          }}
-        />
-        <Text>Enter Age</Text>
-        <TextInput
-          keyboardType="numeric"
-          style={styles.textInput}
-          placeholder="Your Age"
-          onChangeText={(val) => {
-            setTextfieldAge(val);
-          }}
-        />
-        <View style={styles.buttonDiv}>
-          <Button
-            title="ENTER"
-            onPress={change}
-            // accessibilityLabel="Learn more about this button"
-          />
-        </View>
-      </View>
-      <View>
-        <Text>
-          Hi I am {name}, age {age}
-        </Text>
-      </View>
+      <Text>HELLO</Text>
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={data}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
     </View>
   );
 }
@@ -53,20 +39,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    margin: 20,
+    padding: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  inputDiv: {
-    alignItems: "center",
+  item: {
+    backgroundColor: "#f0f0f0",
+    padding: 20,
   },
-  buttonDiv: {
-    margin: 10,
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
+  // inputDiv: {
+  //   alignItems: "center",
+  // },
+  // buttonDiv: {
+  //   margin: 10,
+  // },
+  // textInput: {
+  //   borderWidth: 1,
+  //   borderColor: "#777",
+  //   padding: 8,
+  //   margin: 10,
+  //   width: 200,
+  // },
 });
